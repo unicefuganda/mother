a3f0d060f6dc7445d1c99779d9facb02a828a8a2
David McCann <david.a.mccann@gmail.com>
2011-10-17 13:16:02 +0300

    added xforms (dependency of uganda_common)

--- mother_project/settings.py
+++ mother_project/settings.py
@@ -15,6 +15,7 @@ sys.path.append(os.path.join(filedir, 'rapidsms_script'))
 sys.path.append(os.path.join(filedir, 'rapidsms_ureport'))
 sys.path.append(os.path.join(filedir, 'rapidsms_uganda_common'))
 sys.path.append(os.path.join(filedir, 'rapidsms_healthmodels'))
+sys.path.append(os.path.join(filedir, 'rapidsms_rapidsms_xforms_src'))
 sys.path.append(os.path.join(filedir, 'django_eav'))
 sys.path.append(os.path.join(filedir, 'mr_src'))
 
