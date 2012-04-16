1cb9eb555c1e382dad41cd38d753c52f76b902c6
Revence Kalibwani <revence@1st.ug>
2012-04-16 15:41:49 +0300

    De-debug.

--- mother_project/mr_src/mr/app.py
+++ mother_project/mr_src/mr/app.py
@@ -38,7 +38,6 @@ class App (AppBase):
           msg = Message(connection = message.connection, status = 'Q', direction = 'O', text = 'You will no longer receive FREE messages from the healthy mothers group. If you want to join again please send JOIN to 6400.')
           msg.save()
           return False
-        raise Exception, settings.KEYWORDS_AND_SLUGS
         if (not message.connection.contact) or (not ScriptProgress.objects.filter(connection = message.connection)):
             message.connection.contact = Contact.objects.create(name='Anonymous User')
             message.connection.contact.last_menses = datetime.now() - timedelta(days = 45)
