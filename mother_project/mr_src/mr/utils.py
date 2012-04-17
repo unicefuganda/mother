4732eb0554c40b4f5498184e718822d997e76f12
Revence Kalibwani <revence@1st.ug>
2012-04-17 09:48:22 +0300

    To make ghost Script*s stop haunting me.

--- mother_project/mr_src/mr/utils.py
+++ mother_project/mr_src/mr/utils.py
@@ -1,6 +1,7 @@
 # vim: ts=4
 from django.contrib.auth.models import User, Group
 from django.conf import settings
+from django.db import transaction
 from poll.models import Poll
 from script.models import Script, ScriptStep, ScriptSession, ScriptProgress
 from script.utils.handling import find_best_response, find_closest_match
@@ -97,238 +98,176 @@ def init_structures(sender, **kwargs):
         pass
 
 def init_autoreg(sender, **kwargs):
-    script, created = Script.objects.get_or_create(
-            slug='mrs_opt_out', defaults={
-            'name':"General opt-out script"})
-    if created:
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            message="You will no longer receive FREE messages from Mother Reminder. If you want to join again please send JOIN to 6400.",
-            order=0,
-            rule=ScriptStep.WAIT_MOVEON,
-            start_offset=0,
-            giveup_offset=60,
-        ))
-    script, created = Script.objects.get_or_create(
-            slug="mrs_autoreg", defaults={
-            'name':"Mother reminder autoregistration script"})
-    if created:
-        user, created = User.objects.get_or_create(username="admin")
-        location_poll = Poll.objects.create
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            poll=Poll.objects.create(
-                user=user, \
-                type=Poll.TYPE_LOCATION, \
-                name='mrs_location',
-                question="Thank you for joining Mother Reminder - a great way for fathers and mothers to get the information they need to have a healthy baby. All messages FREE!",
-                default_response='', \
-            ),
-            order=0,
-            rule=ScriptStep.STRICT_MOVEON,
-            start_offset=0,
-            retry_offset=3600,
-            num_tries=2,
-            giveup_offset=86400,
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            poll=Poll.objects.create(
-                user=user,
-                type=Poll.TYPE_NUMERIC,
-                name='mrs_mensesweeks',
-                question='Thank you! How long ago was the last menses? Please reply with the number of weeks that have passed since the last menses.',
-                default_response=''
-            ),
-            order=1,
-            rule=ScriptStep.STRICT_MOVEON,
-            start_offset=0,
-            retry_offset=3600,
-            num_tries=2,
-            giveup_offset=86400,
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            poll=Poll.objects.create(
-                user=user,
-                type=Poll.TYPE_NUMERIC,
-                name='mrs_anc_visits',
-                question="You're almost done! One last question: how many times did you visit the clinic during pregnancy? Please reply with the number of visits you have made.",
-                default_response=''
-            ),
-            order=2,
-            rule=ScriptStep.STRICT_MOVEON,
-            start_offset=0,
-            retry_offset=3600,
-            num_tries=2,
-            giveup_offset=86400,
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            message='Thank you for answering all the questions. You will now receive weekly information about keeping a healthy lifestyle for you and your child, all messages FREE!',
-            rule=ScriptStep.WAIT_MOVEON,
-            order=3,
-            start_offset=0
-        ))
-    script, created  = Script.objects.get_or_create(
-            slug="mrs_hw_autoreg", defaults={
-            'name':"Health worker auto-registration script."})
-    if created:
-        user, created = User.objects.get_or_create(username="admin")
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            message="Welcome to the Health Worker registration for Mother Reminder. Please answer the questions in the following messages.",
-            order=0,
-            rule=ScriptStep.WAIT_MOVEON,
-            start_offset=0,
-            giveup_offset=60,
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script = script,
-            poll   = Poll.objects.create(user = user, type = Poll.TYPE_TEXT,
-                        name     = 'hw_district',
-                        question = 'What is the name of your district?',
-                default_response = ''),
-            order         = 1,
-            rule          = ScriptStep.STRICT_MOVEON,
-            start_offset  = 0,
-            retry_offset  = 3600,
-            num_tries     = 1,
-            giveup_offset = 86400
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script = script,
-            poll   = Poll.objects.create(user = user, type = Poll.TYPE_TEXT,
-                            name = 'hw_healthcentre',
-                        question = 'What is the name of your Health Centre?',
-                default_response = ''),
-            order           = 2,
-            rule            = ScriptStep.STRICT_MOVEON,
-            start_offset    = 0,
-            retry_offset    = 3600,
-            num_tries       = 1,
-            giveup_offset   = 86400
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script  = script,
-               poll = Poll.objects.create(user = user, type = Poll.TYPE_TEXT,
-                        name = 'hw_hclevel',
-                    question = 'What is the level of your Health Centre?',
-            default_response = ''),
-              order = 3,
-               rule = ScriptStep.STRICT_MOVEON,
-       start_offset = 0,
-       retry_offset = 3600,
-          num_tries = 1,
-      giveup_offset = 86400
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script  =   script,
-            poll    =   Poll.objects.create(user=user, type=Poll.TYPE_TEXT,
-                        name    =   'hw_name',
-                    question    =   'What is your name?',
-            default_response    =   ''
+    script = None
+    try:
+        script  =   Script.objects.get(slug = 'mrs_opt_out')
+        script.delete()
+    except Script.DoesNotExist:
+        pass
+    script = Script.objects.create(slug =   'mrs_opt_out',
+                                   name = "General opt-out script")
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        message="You will no longer receive FREE messages from Mother Reminder. If you want to join again please send JOIN to 6400.",
+        order=0,
+        rule=ScriptStep.WAIT_MOVEON,
+        start_offset=0,
+        giveup_offset=60,
+    ))
+    script = None
+    try:
+        script  =   Script.objects.get(slug = 'mrs_autoreg')
+        script.delete()
+    except Script.DoesNotExist:
+        pass
+    script  = Script.objects.create(slug = "mrs_autoreg",
+                                    name = "Mother reminder autoregistration script")
+    user, created = User.objects.get_or_create(username="admin")
+    location_poll = Poll.objects.create
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        poll=Poll.objects.create(
+            user=user, \
+            type=Poll.TYPE_LOCATION, \
+            name='mrs_location',
+            question="Thank you for joining Mother Reminder - a great way for fathers and mothers to get the information they need to have a healthy baby. All messages FREE!",
+            default_response='', \
         ),
-            order           =   4,
-            rule            =   ScriptStep.RESEND_MOVEON,
-            num_tries       =   1,
-            start_offset    =   0,
-            retry_offset    =   3600,
-            giveup_offset   =   86400
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            message="You successfully registered as a Health Worker with Mother Reminder. You will be occasionally asked to provide certain information.",
-            order=5,
-            rule=ScriptStep.WAIT_MOVEON,
-            start_offset=0,
-            giveup_offset=0,
-        ))
-        if 'django.contrib.sites' in settings.INSTALLED_APPS:
-            from django.contrib.sites.models import Site
-            script.sites.add(Site.objects.get_current())
-            for poll in Poll.objects.filter(user = user):
-                poll.sites.add(Site.objects.get_current())
-    script, created  = Script.objects.get_or_create(
-            slug="mrs_hw_reminder", defaults={
-            'name':"Health worker reminder script."})
-    if created:
-        user, created = User.objects.get_or_create(username="admin")
+        order=0,
+        rule=ScriptStep.STRICT_MOVEON,
+        start_offset=0,
+        retry_offset=3600,
+        num_tries=2,
+        giveup_offset=86400,
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        poll=Poll.objects.create(
+            user=user,
+            type=Poll.TYPE_NUMERIC,
+            name='mrs_mensesweeks',
+            question='Thank you! How long ago was the last menses? Please reply with the number of weeks that have passed since the last menses.',
+            default_response=''
+        ),
+        order=1,
+        rule=ScriptStep.STRICT_MOVEON,
+        start_offset=0,
+        retry_offset=3600,
+        num_tries=2,
+        giveup_offset=86400,
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        poll=Poll.objects.create(
+            user=user,
+            type=Poll.TYPE_NUMERIC,
+            name='mrs_anc_visits',
+            question="You're almost done! One last question: how many times did you visit the clinic during pregnancy? Please reply with the number of visits you have made.",
+            default_response=''
+        ),
+        order=2,
+        rule=ScriptStep.STRICT_MOVEON,
+        start_offset=0,
+        retry_offset=3600,
+        num_tries=2,
+        giveup_offset=86400,
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        message='Thank you for answering all the questions. You will now receive weekly information about keeping a healthy lifestyle for you and your child, all messages FREE!',
+        rule=ScriptStep.WAIT_MOVEON,
+        order=3,
+        start_offset=0
+    ))
+    script = None
+    try:
+        script  =   Script.objects.get(slug = 'mrs_hw_autoreg')
+        script.delete()
+    except Script.DoesNotExist:
+        pass
+    script  = Script.objects.create(slug  =   "mrs_hw_autoreg",
+                                    name  =   "Health worker auto-registration script.")
+    user, created = User.objects.get_or_create(username="admin")
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        message="Welcome to the Health Worker registration for Mother Reminder. Please answer the questions in the following messages.",
+        order=0,
+        rule=ScriptStep.WAIT_MOVEON,
+        start_offset=0,
+        giveup_offset=60,
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script = script,
+        poll   = Poll.objects.create(user = user, type = Poll.TYPE_TEXT,
+                    name     = 'hw_district',
+                    question = 'What is the name of your district?',
+            default_response = ''),
+        order         = 1,
+        rule          = ScriptStep.STRICT_MOVEON,
+        start_offset  = 0,
+        retry_offset  = 3600,
+        num_tries     = 1,
+        giveup_offset = 86400
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script = script,
+        poll   = Poll.objects.create(user = user, type = Poll.TYPE_TEXT,
+                        name = 'hw_healthcentre',
+                    question = 'What is the name of your Health Centre?',
+            default_response = ''),
+        order           = 2,
+        rule            = ScriptStep.STRICT_MOVEON,
+        start_offset    = 0,
+        retry_offset    = 3600,
+        num_tries       = 1,
+        giveup_offset   = 86400
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script  = script,
+           poll = Poll.objects.create(user = user, type = Poll.TYPE_TEXT,
+                    name = 'hw_hclevel',
+                question = 'What is the level of your Health Centre?',
+        default_response = ''),
+          order = 3,
+           rule = ScriptStep.STRICT_MOVEON,
+   start_offset = 0,
+   retry_offset = 3600,
+      num_tries = 1,
+  giveup_offset = 86400
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script  =   script,
+        poll    =   Poll.objects.create(user=user, type=Poll.TYPE_TEXT,
+                    name    =   'hw_name',
+                question    =   'What is your name?',
+        default_response    =   ''
+    ),
+        order           =   4,
+        rule            =   ScriptStep.RESEND_MOVEON,
+        num_tries       =   1,
+        start_offset    =   0,
+        retry_offset    =   3600,
+        giveup_offset   =   86400
+    ))
+    script.steps.add(ScriptStep.objects.create(
+        script=script,
+        message="You successfully registered as a Health Worker with Mother Reminder. You will be occasionally asked to provide certain information.",
+        order=5,
+        rule=ScriptStep.WAIT_MOVEON,
+        start_offset=0,
+        giveup_offset=0,
+    ))
+    if 'django.contrib.sites' in settings.INSTALLED_APPS:
+        from django.contrib.sites.models import Site
+        script.sites.add(Site.objects.get_current())
+        for poll in Poll.objects.filter(user = user):
+            poll.sites.add(Site.objects.get_current())
 
-        script.steps.add(ScriptStep.objects.create(
-            script = script,
-            poll   = Poll.objects.create(user = user, type = Poll.TYPE_NUMERIC,
-                        name     = 'hw_question_1',
-                        question = 'How many women came for their first ANC visit this month?',
-                default_response = ''),
-            order         = 0,
-            rule          = ScriptStep.STRICT_MOVEON,
-            start_offset  = 0,
-            retry_offset  = 86400 * 2,
-            num_tries     = 7,
-            giveup_offset = 86400 * 15
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script = script,
-            poll   = Poll.objects.create(user = user, type = Poll.TYPE_NUMERIC,
-                        name     = 'hw_question_2',
-                        question = 'How many women came for their fourth ANC visit this month?',
-                default_response = ''),
-            order         = 1,
-            rule          = ScriptStep.STRICT_MOVEON,
-            start_offset  = 0,
-            retry_offset  = 86400 * 2,
-            num_tries     = 7,
-            giveup_offset = 86400 * 15
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script = script,
-            poll   = Poll.objects.create(user = user, type = Poll.TYPE_NUMERIC,
-                        name     = 'hw_question_3',
-                        question = 'How many HIV+ mothers are receiving anti-retroviral treatment?',
-                default_response = ''),
-            order         = 2,
-            rule          = ScriptStep.STRICT_MOVEON,
-            start_offset  = 0,
-            retry_offset  = 86400 * 2,
-            num_tries     = 7,
-            giveup_offset = 86400 * 15
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script = script,
-            poll   = Poll.objects.create(user = user, type = Poll.TYPE_NUMERIC,
-                        name     = 'hw_question_4',
-                        question = 'How many infants returned for 6-week checkup for early infant HIV diagnosis?',
-                default_response = ''),
-            order         = 3,
-            rule          = ScriptStep.STRICT_MOVEON,
-            start_offset  = 0,
-            retry_offset  = 86400 * 2,
-            num_tries     = 7,
-            giveup_offset = 86400 * 15
-        ))
-        script.steps.add(ScriptStep.objects.create(
-            script=script,
-            message="Thank you for sending us that information.",
-            order=4,
-            rule=ScriptStep.WAIT_MOVEON,
-            start_offset=0,
-            giveup_offset=0,
-        ))
-        if 'django.contrib.sites' in settings.INSTALLED_APPS:
-            from django.contrib.sites.models import Site
-            script.sites.add(Site.objects.get_current())
-            for poll in Poll.objects.filter(user = user):
-                poll.sites.add(Site.objects.get_current())
-
-
-
-def reminders(last_menses):
-    toret = []
-    if last_menses:
-        offset = timedelta(90)
-        curtime = last_menses + offset
-        for m in ["It's time for your first trimester visit!", "It's time for your second trimester visit!", "It's time for your third trimester visit!", "Please see your doctor for antenatal care"]:
-            toret.append((curtime, m,))
-            curtime = curtime + offset
-    return toret
+@transaction.commit_manually
+def throw_down_scripts():
+    'This is only for bad days. Like today.'
+    ScriptSession.objects.all().delete()
+    ScriptProgress.objects.all().delete()
+    ScriptStep.objects.all().delete()
+    Script.objects.all().delete()
+    transaction.commit()
