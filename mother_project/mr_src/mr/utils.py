dfd9a05b023aef8d27e269e6cf899b594e7420bb
Revence Kalibwani <revence@1st.ug>
2012-04-30 19:38:17 +0300

    Fetch pertinent poll.

--- mother_project/mr_src/mr/utils.py
+++ mother_project/mr_src/mr/utils.py
@@ -97,8 +97,9 @@ def mr_autoreg(**kwargs):
 def check_for_validity(progress):
   step    = progress.script.steps.get(poll__name = 'mrs_location')
   try:
-    session = ScriptSession.objects.filter(connection = progress.connection, end_time = None)[0]
-    loc     = find_best_response(session, location_poll)
+    session       = ScriptSession.objects.filter(connection = progress.connection, end_time = None)[0]
+    location_poll = progress.script.steps.get(poll__name='mrs_location').poll
+    loc           = find_best_response(session, location_poll)
     if not loc: return False
     return loc.type.stub == 'district'
   except IndexError:
